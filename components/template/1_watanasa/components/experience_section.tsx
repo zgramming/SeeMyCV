23bd7697499a7a47487997b45cf6f3ff33818751
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-11-12 13:39:47 +0700

    Feat: Modal Experience

--- components/template/1_watanasa/components/experience_section.tsx
+++ components/template/1_watanasa/components/experience_section.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Col, Row, Tag } from "antd";
+import { Button, Card, Col, Modal, Row, Tag } from "antd";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
@@ -29,6 +29,8 @@ const ExperienceItem = ({
   experience: CVExperienceInterface;
 }) => {
   const [tags, setTags] = useState<string[] | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
   const startDate = new Date(experience.start_date);
   const endDate = experience.end_date && new Date(experience.end_date);
   const calculating = calculatingExperience(startDate, endDate ?? new Date());
@@ -45,56 +47,90 @@ const ExperienceItem = ({
     return () => {};
   }, [experience.tags]);
 
+  const Calendar = () => {
+    return (
+      <div className="flex flex-row items-center text-xs space-x-2">
+        <CalendarOutlined />
+        <span>
+          {dateToyMd(startDate)} - {endDate ? dateToyMd(endDate) : "Sekarang"}{" "}
+          {calculating}
+        </span>
+      </div>
+    );
+  };
+
+  const TagList = () => {
+    return (
+      <div className="flex flex-wrap pt-2">
+        {tags &&
+          tags.map((val, index) => {
+            return (
+              <Tag
+                key={val}
+                className="bg-watanasa-primary-100 text-watanasa-primary-500 font-bold my-1 p-1 rounded border-none"
+              >
+                {val}
+              </Tag>
+            );
+          })}
+      </div>
+    );
+  };
+
   return (
-    <Card className={"rounded-xl"} hoverable>
-      <Row align="top">
-        {hasImage && (
-          <Col sm={24} md={24} lg={4} className="w-full hidden lg:block">
-            <div className="relative w-full h-16 bg-red-50 xl:h-20">
-              <Image
-                alt="Image Experience"
-                src={experience.image_company ?? ""}
-                className={"rounded-xl shadow-xl"}
-                style={{ objectFit: "cover" }}
-                fill
+    <>
+      <Card className={"rounded-xl"} onClick={(e) => setIsOpen(true)} hoverable>
+        <Row align="top">
+          {hasImage && (
+            <Col sm={24} md={24} lg={4} className="w-full hidden lg:block">
+              <div className="relative w-full h-16 bg-red-50 xl:h-20">
+                <Image
+                  alt="Image Experience"
+                  src={experience.image_company ?? ""}
+                  className={"rounded-xl shadow-xl"}
+                  style={{ objectFit: "cover" }}
+                  fill
+                />
+              </div>
+            </Col>
+          )}
+          <Col
+            sm={24}
+            md={24}
+            lg={hasImage ? 20 : 24}
+            className="w-full lg:pl-5"
+          >
+            <div className="flex flex-col space-y-1 text-watanasa-gray-1">
+              <div className="font-semibold text-xl">{experience.company}</div>
+              <div className="font-medium text-watanasa-gray-3 text-base">
+                {experience.job}
+              </div>
+              <Calendar />
+              <div
+                className={`text-sm pt-3 line-clamp-2`}
+                dangerouslySetInnerHTML={{ __html: experience.description }}
               />
+              <TagList />
             </div>
           </Col>
-        )}
-        <Col sm={24} md={24} lg={hasImage ? 20 : 24} className="w-full lg:pl-5">
-          <div className="flex flex-col space-y-1 text-watanasa-gray-1">
-            <div className="font-semibold text-xl">{experience.company}</div>
-            <div className="font-medium text-watanasa-gray-3 text-base">
-              {experience.job}
-            </div>
-            <div className="flex flex-row items-center text-xs space-x-2">
-              <CalendarOutlined />
-              <span>
-                {dateToyMd(startDate)} -{" "}
-                {endDate ? dateToyMd(endDate) : "Sekarang"} {calculating}
-              </span>
-            </div>
-            <div
-              className="text-sm pt-3 line-clamp-2"
-              dangerouslySetInnerHTML={{ __html: experience.description }}
-            />
-            <div className="flex flex-wrap pt-2">
-              {tags &&
-                tags.map((val, index) => {
-                  return (
-                    <Tag
-                      key={val}
-                      className="bg-watanasa-primary-100 text-watanasa-primary-500 font-bold my-1 p-1 rounded border-none"
-                    >
-                      {val}
-                    </Tag>
-                  );
-                })}
-            </div>
+        </Row>
+      </Card>
+      <Modal
+        title={
+          <div className="flex flex-col space-y-2">
+            <div>{experience.company}</div>
+            <div className="text-watanasa-primary-500">{experience.job}</div>
+            <Calendar />
+            <TagList />
           </div>
-        </Col>
-      </Row>
-    </Card>
+        }
+        open={isOpen}
+        onCancel={(e) => setIsOpen(false)}
+        footer={null}
+      >
+        <div dangerouslySetInnerHTML={{ __html: experience.description }} />
+      </Modal>
+    </>
   );
 };
 
