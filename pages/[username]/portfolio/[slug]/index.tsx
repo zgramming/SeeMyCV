cc25e5a21eff132b234841874d433a1c5f652603
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-11-12 19:52:22 +0700

    Feat: Adding meta tag on portfolio detail

--- pages/[username]/portfolio/[slug]/index.tsx
+++ pages/[username]/portfolio/[slug]/index.tsx
@@ -1,6 +1,7 @@
 import { Tag } from "antd";
 import axios from "axios";
 import { GetServerSideProps } from "next";
+import Head from "next/head";
 import { env } from "process";
 import { ReactElement } from "react";
 
@@ -9,52 +10,69 @@ import { CVPortfolioInterface } from "../../../../interface/cv/cvportfolio_inter
 
 const Page = ({ portfolio }: { portfolio: CVPortfolioInterface }) => {
   const createdDate = new Date(portfolio.created_at);
+  const url = `${process.env.NEXT_PUBLIC_BASEURL}`;
+  const imageUrl = `${process.env.NEXT_PUBLIC_BASEURL}/images/logo_primary.png`;
+
   return (
-    <div className="min-h-[50vh] flex flex-col space-y-5 py-12 px-5 md:px-12 lg:px-24">
-      <div className="text-watanasa-gray-3 text-base">
-        {createdDate.toLocaleDateString("id-ID", {
-          month: "long",
-          day: "2-digit",
-          year: "numeric",
-        })}
-      </div>
-      <div className="font-medium text-4xl">{portfolio.title}</div>
-      <div
-        dangerouslySetInnerHTML={{ __html: portfolio.description }}
-        className={"text-base font-normal text-justify leading-relaxed"}
-      />
-      <div className="flex flex-col space-y-3">
-        <div className="font-medium text-xl">Social Media</div>
-        <div className="flex flex-wrap items-center space-x-5">
-          {portfolio.urls.map((val, index) => {
-            return (
-              <a
-                key={val.id}
-                className="font-medium underline text-watanasa-gray-1 text-base"
-                onClick={(e) => window.open(val.url)}
-              >
-                {val.name}
-              </a>
-            );
+    <>
+      <Head>
+        <title>{portfolio.title}</title>
+        <meta name="description" content={portfolio.description} />
+        <meta property="og:title" content={portfolio.title} />
+        <meta property="og:type" content={portfolio.slug} />
+        <meta property="og:url" content={url} />
+        <meta property="og:image:url" content={imageUrl} />
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="400" />
+        <meta property="og:image:alt" content="SeeMyCV" />
+        <meta property="og:description" content={portfolio.description} />
+      </Head>
+      <div className="min-h-[50vh] flex flex-col space-y-5 py-12 px-5 md:px-12 lg:px-24">
+        <div className="text-watanasa-gray-3 text-base">
+          {createdDate.toLocaleDateString("id-ID", {
+            month: "long",
+            day: "2-digit",
+            year: "numeric",
           })}
         </div>
-      </div>
-      <div className="flex flex-col space-y-3">
-        <div className="font-medium text-xl">Tags</div>
-        <div className="flex flex-wrap items-center space-x-3">
-          {portfolio.urls.map((val, index) => {
-            return (
-              <Tag
-                key={val.id}
-                className="bg-watanasa-primary-100 text-watanasa-primary-500 text-base font-semibold border-none shadow rounded mb-1 p-2"
-              >
-                {val.name}
-              </Tag>
-            );
-          })}
+        <div className="font-medium text-4xl">{portfolio.title}</div>
+        <div
+          dangerouslySetInnerHTML={{ __html: portfolio.description }}
+          className={"text-base font-normal text-justify leading-relaxed"}
+        />
+        <div className="flex flex-col space-y-3">
+          <div className="font-medium text-xl">Social Media</div>
+          <div className="flex flex-wrap items-center space-x-5">
+            {portfolio.urls.map((val, index) => {
+              return (
+                <a
+                  key={val.id}
+                  className="font-medium underline text-watanasa-gray-1 text-base"
+                  onClick={(e) => window.open(val.url)}
+                >
+                  {val.name}
+                </a>
+              );
+            })}
+          </div>
+        </div>
+        <div className="flex flex-col space-y-3">
+          <div className="font-medium text-xl">Tags</div>
+          <div className="flex flex-wrap items-center space-x-3">
+            {portfolio.urls.map((val, index) => {
+              return (
+                <Tag
+                  key={val.id}
+                  className="bg-watanasa-primary-100 text-watanasa-primary-500 text-base font-semibold border-none shadow rounded mb-1 p-2"
+                >
+                  {val.name}
+                </Tag>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
