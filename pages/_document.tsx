12791503de30753ebc269ff84911c9269d213f94
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-11-13 09:43:52 +0700

    Fix : Adding lang prop to html tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="id">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
