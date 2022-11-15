930098d7226ad3fe4d6eae5e60663c754d42b6d5
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-11-15 21:48:11 +0700

    Feat: Adding logo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400;500;600;700&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/images/logo_primary.png" />
       </Head>
       <body>
         <Main />
