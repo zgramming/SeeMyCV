b34081e98b4db6f49bae0af58a6e27877d5279c8
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-11-23 20:12:35 +0700

    Feat : Naraai Template Section Portfolio

--- components/template/2_naraai/naraai_template.tsx
+++ components/template/2_naraai/naraai_template.tsx
@@ -270,7 +270,68 @@ const SkillSection = () => {
 };
 
 const PortfolioSection = () => {
-  return <></>;
+  return (
+    <div className="flex flex-col px-5 md:px-12 lg:px-24">
+      <div className="font-semibold text-naraai-gray-3 text-2xl text-center pb-3">
+        PORTFOLIO
+      </div>
+      <div className="text-center text-naraai-primary-500 text-4xl font-bold pb-8">
+        LATEST PROJECT
+      </div>
+      <div className="grid grid-cols-1 gap-10 xl:grid-cols-4">
+        {Array.from<number>({ length: 10 }).map((val) => {
+          return (
+            <Card
+              key={val}
+              style={{
+                padding: 0,
+                margin: 0,
+              }}
+              bodyStyle={{
+                padding: 0,
+                margin: 0,
+              }}
+              className="rounded-xl"
+            >
+              <div className="flex flex-col space-y-5 pb-6">
+                <div className="relative h-64">
+                  <Image
+                    alt="Image Portfolio"
+                    src={`https://picsum.photos/800`}
+                    className="shadow-lg rounded-tl-xl rounded-tr-xl"
+                    style={{
+                      objectFit: "cover",
+                    }}
+                    fill
+                  />
+                </div>
+                <div className="flex flex-col space-y-5 px-4">
+                  <div className="text-naraai-gray-1 text-base font-medium">
+                    How to prototype in figma
+                  </div>
+                  <div className="self-start bg-naraai-primary-100 text-naraai-primary-500 text-xs font-medium rounded-xl p-2">
+                    July 15, 2021
+                  </div>
+                  <div className="text-naraai-gray-3 text-base h-24 overflow-hidden">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Sapiente, fuga consequatur veniam dicta fugit cum harum hic
+                    optio beatae quis at minima ipsa odit, iure distinctio! Nam
+                    error dolorum reprehenderit.
+                  </div>
+                  <Button
+                    className="self-start text-naraai-primary-500 font-bold"
+                    type="link"
+                  >
+                    Continue Reading
+                  </Button>
+                </div>
+              </div>
+            </Card>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 const LicenseCertificateSection = () => {
