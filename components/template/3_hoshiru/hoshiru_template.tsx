14106f2d768438ca7099b16110c32ac3f87da6fd
Zeffry Reynando <zeffry.reynando@gmail.com>
2022-12-11 15:09:52 +0700

    Feat : Education Section Template Hoshiru

--- components/template/3_hoshiru/hoshiru_template.tsx
+++ components/template/3_hoshiru/hoshiru_template.tsx
@@ -1,20 +1,10 @@
 import { useEffect, useRef } from "react";
-import { CVEducationInterface } from "../../../interface/cv/cveducation_interface";
 
 import { Users } from "../../../interface/main_interface";
 import navigationScrollToComponent from "../../../repository/navigation_scrollto_component";
+import EducationSection from "./components/education_section";
 import ExperienceSection from "./components/experience_section";
 import ProfileSection from "./components/profile_section";
-
-const EducationSection = ({
-  educations,
-}: {
-  educations: CVEducationInterface[];
-}) => {
-  return (
-    <div className="flex flex-col space-y-10 bg-hoshiru-primary-100 px-5 md:px-12 lg:py-12 lg:px-24"></div>
-  );
-};
 
 const HoshiruTemplate = ({ user }: { user: Users }) => {
   const profileRef = useRef(null);
@@ -66,4 +56,5 @@ const HoshiruTemplate = ({ user }: { user: Users }) => {
     </div>
   );
 };
+
 export default HoshiruTemplate;
